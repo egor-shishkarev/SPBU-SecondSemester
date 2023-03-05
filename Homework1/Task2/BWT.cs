1e7664b44cef28b6f8a56334128fc8307a0b45df
egor-shishkarev <egorsiskarev92@gmail.com>
2023-03-05 17:03:50 +0300

    Add and rename files, fix conversion error, add new tests .

---
+++ Homework1/Task2/BWT.cs
@@ -0,0 +1,65 @@
+﻿namespace Transform;
+
+using System.Text;
+
+public static class BWT
+{
+    public static (StringBuilder BWTString, int lastPosition) DirectBWT(string stringToConvert)
+    {
+        if (stringToConvert == null)
+        {
+            return (new StringBuilder(0), 0);
+        }
+        stringToConvert += "\0";
+        (var arrayOfIndex, bool notNull) = Sorting.BubbleSuffixSort(stringToConvert);
+        if (!notNull)
+        {
+            return (new StringBuilder(0), 0);
+        }
+        var BWTString = new StringBuilder(stringToConvert.Length);
+        for (int i = 0; i < stringToConvert.Length; ++i)
+        {
+            BWTString.Append(stringToConvert[(stringToConvert.Length + arrayOfIndex[i] - 1) % stringToConvert.Length]);
+        }
+        int lastPosition = BWTString.ToString().IndexOf('\0');
+        BWTString = BWTString.Replace("\0", "");
+        return (BWTString, lastPosition);
+    }
+
+    public static StringBuilder ReverseBWT(StringBuilder BWTString, int lastPosition)
+    {
+        StringBuilder BWTStringWithAdditionalSymbol = new(BWTString.Length + 1);
+        for (int i = 0; i < BWTString.Length; ++i)
+        {
+            BWTStringWithAdditionalSymbol.Append(BWTString[i]);
+        }
+        BWTStringWithAdditionalSymbol.Insert(lastPosition, '\0');
+        var arrayOfChars = BWTStringWithAdditionalSymbol.ToString().ToCharArray();
+        var arrayOfIndex = new int[arrayOfChars.Length];
+        for (int i = 0; i < arrayOfChars.Length; ++i)
+        {
+            arrayOfIndex[i] = i;
+        }
+        for (int count = 0; count < arrayOfChars.Length; ++count)
+        {
+            for (int j = 1; j < arrayOfChars.Length; ++j)
+            {
+                int i = j - 1;
+                if (arrayOfChars[i] > arrayOfChars[j])
+                {
+                    (arrayOfIndex[j], arrayOfIndex[i]) = (arrayOfIndex[i], arrayOfIndex[j]);
+                    (arrayOfChars[j], arrayOfChars[i]) = (arrayOfChars[i], arrayOfChars[j]);
+                }
+            }
+        }
+        int currentIndex = 0;
+        StringBuilder reverseBWTString = new(arrayOfChars.Length);
+        for (int i = 0; i < arrayOfChars.Length; ++i)
+        {
+            reverseBWTString.Append(arrayOfChars[currentIndex]);
+            currentIndex = arrayOfIndex[currentIndex];
+        }
+        reverseBWTString.Replace("\0", "");
+        return reverseBWTString;
+    }
+}
---
+++ Homework1/Task2/BWTTest.cs
@@ -0,0 +1,23 @@
+﻿using Transform;
+using System.Text;
+
+public static class BWTTest
+{
+    public static bool Test()
+    {
+        string[] tests = { "banana", "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES", "0c\\ab", "\\cba0", "\\0", "0\\" , "ехал грека через реку видит грека в реке рак сунул грека руку в реку рак за руку грека цап" };
+        foreach (string testCase in tests)
+        {
+            (StringBuilder BWTString, int lastPosition) = BWT.DirectBWT(testCase);
+            if (BWTString.Length != testCase.Length && lastPosition == 0)
+            {
+                return false;
+            }
+            if (String.Compare(testCase, BWT.ReverseBWT(BWTString, lastPosition).ToString()) != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
