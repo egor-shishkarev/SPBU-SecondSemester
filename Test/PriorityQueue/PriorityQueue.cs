650707c0a20ebe1606d4b293cb13b6f445811550
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-14 14:35:32 +0300

    Add copyright

--- Test/PriorityQueue/PriorityQueue.cs
+++ Test/PriorityQueue/PriorityQueue.cs
@@ -1,4 +1,19 @@
-﻿namespace Queue;
+﻿/*Copyright 2023 Egor Shishkarev
+*
+*Licensed under the Apache License, Version 2.0 (the "License");
+*you may not use this file except in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing, software
+*distributed under the License is distributed on an "AS IS" BASIS,
+*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*See the License for the specific language governing permissions and
+*limitations under the License.
+*/
+
+namespace Queue;
 
 /// <summary>
 /// Class of priority queue based on List.
