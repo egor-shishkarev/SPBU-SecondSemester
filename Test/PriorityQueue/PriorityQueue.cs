1738206ceb13953bf34cb914ccc4f9922aed6c21
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-14 14:12:38 +0300

    Add priorityQueue class and tests

---
+++ Test/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿namespace Queue;
+
+public class PriorityQueue
+{
+    readonly List<(int priority, int value)> head;
+    public PriorityQueue()
+    {
+        head = new List<(int, int)>();
+    }
+
+    public void Enqueue(int value, int priority)
+    {
+        if (priority < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(priority), "Priority should be positive");
+        }
+        head.Add((priority, value));
+    }
+
+    public int Dequeue()
+    {
+        if (Empty)
+        {
+            throw new ArgumentException("Queue has no elements");
+        }
+        var maxPriority = int.MinValue;
+        var indexOfMaxPriority = 0;
+        for (int i = 0; i < head.Count; ++i)
+        {
+            if (head[i].priority > maxPriority)
+            {
+                maxPriority = head[i].priority;
+                indexOfMaxPriority = i;
+            }
+        }
+        var result = head[indexOfMaxPriority].value;
+        head.RemoveAt(indexOfMaxPriority);
+        return result;
+    }
+
+    public bool Empty => head.Count == 0;
+}
+
+/*Реализовать очередь с приоритетами. Очередь должна иметь метод Enqueue(), 
+ * принимающий на вход значение и численный приоритет, метод Dequeue(), 
+ * возвращающий значение с наибольшим приоритетом (то есть приоритетом с 
+ * наибольшим численным значением) и удаляющий его из очереди, и свойство 
+ * Empty, возвращающее, пуста ли очередь. Если приоритеты элементов равны, 
+ * они должны возвращаться в порядке, в котором они поступили в очередь 
+ * (First In — First Out). Тип хранимых значений — любой, на ваш выбор. 
+ * Если очередь пуста, Dequeue() должен бросать исключение. Комментарии, 
+ * юнит-тесты и CI также ожидаются от правильного решения.*/
---
+++ Test/QueueTests/QueueTests.cs
@@ -0,0 +1,37 @@
+namespace Queue.Tests;
+
+public class QueueTests
+{
+    [Test]
+    public void DequeueWithEmptyQueueShouldThrowException()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        Assert.Throws<ArgumentException>(() => priorityQueue.Dequeue());
+    }
+
+    [Test]
+    public void EnqueueWithNegativePrioritiesShouldThrowException()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => priorityQueue.Enqueue(1, -10));
+    }
+
+    [Test]
+    public void DequeueShouldReturnExpectedResul()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(1, 5);
+        priorityQueue.Enqueue(2, 1);
+        priorityQueue.Enqueue(0, 2);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(priorityQueue.Dequeue(), Is.EqualTo(1));
+            Assert.That(priorityQueue.Dequeue(), Is.EqualTo(0));
+            Assert.That(priorityQueue.Dequeue(), Is.EqualTo(2));
+            Assert.That(priorityQueue.Empty, Is.True);
+        });
+    }
+}
