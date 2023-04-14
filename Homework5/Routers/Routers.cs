b374c32ab9664972a3c63cab76bbfc9673b34dc6
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-14 13:30:09 +0300

    Add classes

---
+++ Homework5/Routers/Routers.cs
@@ -0,0 +1,6 @@
+﻿namespace Routers;
+
+public class Routers
+{
+
+}
---
+++ Homework5/RoutersTests/RoutersTests.cs
@@ -0,0 +1,16 @@
+namespace RoutersTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
