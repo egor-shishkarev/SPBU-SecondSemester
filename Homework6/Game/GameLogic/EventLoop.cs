1244571c5ffca3041dea8886cb7edf7bd3fa1bdd
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-21 02:27:24 +0300

    Add coins and score

--- Homework6/Game/GameLogic/EventLoop.cs
+++ Homework6/Game/GameLogic/EventLoop.cs
@@ -24,19 +24,49 @@ public class EventLoop
         {
             Console.WriteLine(line);
         }
-        Console.SetCursorPosition(1, 1);
+        Console.SetCursorPosition(45, 0);
+        Console.WriteLine("Ваш счёт: 0");
+        Console.SetCursorPosition(20, 9);
         Console.Write("@");
-        Console.SetCursorPosition(1, 1);
+        Console.SetCursorPosition(20, 9);
         Console.CursorVisible = false;
+        (int rowNumberCoin, int columnNumberCoin) = GetRandomPosition(walls);
+        Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
+        Console.Write(" \b");
+        Console.Write("$");
+        Console.SetCursorPosition(20, 9);
+        int score = 0;
         while (true)
         {
+            if (Console.GetCursorPosition() == (columnNumberCoin, rowNumberCoin))
+            {
+                var currentRow = rowNumberCoin;
+                var currentColumn = columnNumberCoin;
+                Console.SetCursorPosition(55, 0);
+                Console.SetCursorPosition(55, 0);
+                for (int i = 0; i < score.ToString().Length; ++i)
+                {
+                    Console.Write(" \b");
+                }
+                score += 500;
+                Console.Write($"{score}");
+                Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
+                
+                (rowNumberCoin, columnNumberCoin) = GetRandomPosition(walls);
+                Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
+                Console.Write(" \b");
+                Console.Write("$");
+                Console.SetCursorPosition(currentColumn, currentRow);
+            }
             var key = Console.ReadKey(true);
             if (key.Key == ConsoleKey.LeftArrow)
             {
                 (int left, int top) = Console.GetCursorPosition();
                 if (walls[top, left - 1] == 1)
                 {
-                    continue;
+                    Console.SetCursorPosition(0, 22);
+                    Console.WriteLine("Вы проиграли");
+                    break;
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left - 1, top);
@@ -48,7 +78,9 @@ public class EventLoop
                 (int left, int top) = Console.GetCursorPosition();
                 if (walls[top, left + 1] == 1)
                 {
-                    continue;
+                    Console.SetCursorPosition(0, 22);
+                    Console.WriteLine("Вы проиграли");
+                    break;
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left + 1, top);
@@ -60,7 +92,9 @@ public class EventLoop
                 (int left, int top) = Console.GetCursorPosition();
                 if (walls[top - 1, left] == 1)
                 {
-                    continue;
+                    Console.SetCursorPosition(0, 22);
+                    Console.WriteLine("Вы проиграли");
+                    break;
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left, top - 1);
@@ -72,7 +106,9 @@ public class EventLoop
                 (int left, int top) = Console.GetCursorPosition();
                 if (walls[top + 1, left] == 1)
                 {
-                    continue;
+                    Console.SetCursorPosition(0, 22);
+                    Console.WriteLine("Вы проиграли");
+                    break;
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left, top + 1);
@@ -80,5 +116,18 @@ public class EventLoop
                 Console.SetCursorPosition(left, top + 1);
             }
         }
+    }
+    private static (int, int) GetRandomPosition(int[,] walls)
+    {
+        Random randomNumeber = new();
+        var rowNumber = randomNumeber.Next(1, 18);
+        var columnNumber = randomNumeber.Next(1, 40);
+        while (walls[rowNumber, columnNumber] != 0)
+        {
+            rowNumber = randomNumeber.Next(1, 18);
+            columnNumber = randomNumeber.Next(1, 40);
+        }
+        return (rowNumber, columnNumber);
+
     }
 }
