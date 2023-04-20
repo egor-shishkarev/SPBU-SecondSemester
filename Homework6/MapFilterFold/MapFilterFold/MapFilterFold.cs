8434e39cd5899612eb17bde99ba73b1d1d785a70
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-20 21:56:47 +0300

    Add comments

--- Homework6/MapFilterFold/MapFilterFold/MapFilterFold.cs
+++ Homework6/MapFilterFold/MapFilterFold/MapFilterFold.cs
@@ -1,7 +1,27 @@
-﻿namespace MapFilterFold;
+﻿// <copyright file = "Program.cs" author = "Egor Shishkarev">
+// Copyright (c) Egor Shishkarev. All rights reserved.
+// </copyright>
 
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace MapFilterFold;
+
+/// <summary>
+/// Class for three methods - Map, Filter and Fold
+/// </summary>
 public class CustomMethods
 {
+    /// <summary>
+    /// Applies the specified function to all elements of the list.
+    /// </summary>
+    /// <typeparam name="InputType">Type of input elements in list.</typeparam>
+    /// <typeparam name="OutputType">Type of output elements in list.</typeparam>
+    /// <param name="listOfElements">The list in which we want to apply the function.</param>
+    /// <param name="function">The function, which we want to apply to elements in list.</param>
+    /// <returns>List after applying the function.</returns>
+    /// <exception cref="ArgumentNullException">List of elements or function were null.</exception>
     public static List<OutputType> Map<InputType, OutputType> (List<InputType> listOfElements, Func<InputType, OutputType> function)
     {
         if (listOfElements == null)
@@ -23,6 +43,14 @@ public class CustomMethods
         return newList;
     }
 
+    /// <summary>
+    /// Selects elements from a list based on a specified function.
+    /// </summary>
+    /// <typeparam name="InputType">Type of input elements in list.</typeparam>
+    /// <param name="listOfElements">The list, where we want to select elements.</param>
+    /// <param name="function">The function with which we select elements from the list.</param>
+    /// <returns>List of elements, selected by function</returns>
+    /// <exception cref="ArgumentNullException">List of elements or function were null.</exception>
     public static List<InputType> Filter<InputType> (List<InputType> listOfElements, Func<InputType, bool> function)
     {
         if (listOfElements == null)
@@ -47,6 +75,16 @@ public class CustomMethods
         return newList;
     }
 
+    /// <summary>
+    /// Selects the current element of the list and executes the function on it and the accumulator.
+    /// </summary>
+    /// <typeparam name="InputType">Type of input elements in list.</typeparam>
+    /// <typeparam name="AccumulatorType">Type of accumulator.</typeparam>
+    /// <param name="listOfElements">The list in which we want to apply the function.</param>
+    /// <param name="initialValue">Initial accumulator value.</param>
+    /// <param name="function">The function, which we want to apply to elements in list and accumulator.</param>
+    /// <returns>Value of accumualtor after applying the function to all elements of the list.</returns>
+    /// <exception cref="ArgumentNullException">List of elements or function were null.</exception>
     public static AccumulatorType Fold<InputType, AccumulatorType>(List<InputType> listOfElements, AccumulatorType initialValue, Func<AccumulatorType, InputType, AccumulatorType> function)
     {
         if (listOfElements == null)
