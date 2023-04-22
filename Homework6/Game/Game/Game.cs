c8881c6f9a9be50b732b4d284a2f8496f28a47b0
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-22 18:59:05 +0300

    Add two new levels and opportunity to use custom maps

--- Homework6/Game/Game/Game.cs
+++ Homework6/Game/Game/Game.cs
@@ -6,5 +6,24 @@ if (args.Length < 1)
     return -1;
 }
 
-Game(args[0]);
+Console.Clear();
+Console.WriteLine("Добро пожаловать в игру - сборщик монет.\nПеред вами появится лабиринт, проходя через который, " +
+    "не касаясь стен вы должны собрать появляющиеся монетки.\nСчет отображается в правом верхнем углу экрана.\nУдачи!");
+if (args.Length == 1)
+{
+    Console.WriteLine("\nДля старта игры нажмите на Enter.");
+    Console.ReadLine();
+    Game(args[0], false);
+}
+
+if (args.Length == 2 && args[1] == "-c")
+{
+    Console.WriteLine("Внимание! Вы играете на персонализированной карте! Играйте на свой страх и риск, " +
+        "так как карта не была проверена программой!");
+    Console.WriteLine("\nДля старта игры нажмите на Enter.");
+    Console.ReadLine();
+    Game(args[0], true);
+} 
+
+
 return 0;
--- Homework6/Game/GameLogic/EventLoop.cs
+++ Homework6/Game/GameLogic/EventLoop.cs
@@ -2,7 +2,7 @@
 
 public class EventLoop
 {
-    public static void Game(string mapPath)
+    public static void Game(string mapPath, bool customMap)
     {
         var map = File.ReadAllLines(mapPath);
         var walls = new int[map.Length, map[0].Length];
@@ -21,22 +21,21 @@ public class EventLoop
             }
         }
         Console.Clear();
-        //Console.WriteLine("Добро пожаловать в игру - сборщик монет.\nПеред вами появится лабиринт, проходя через который, не касаясь стен вы должны собрать появляющиеся монетки.\nСчет отображается в правом верхнем углу экрана.\nУдачи!\n");
         foreach (var line in map)
         {
             Console.WriteLine(line);
         }
         Console.SetCursorPosition(45, 0);
         Console.WriteLine("Ваш счёт: 0");
-        Console.SetCursorPosition(20, 9);
+        Console.SetCursorPosition(1, 1);
         Console.Write("@");
-        Console.SetCursorPosition(20, 9);
+        Console.SetCursorPosition(1, 1);
         Console.CursorVisible = false;
-        (int rowNumberCoin, int columnNumberCoin) = GetRandomPosition(walls);
+        (int rowNumberCoin, int columnNumberCoin) = GetRandomPosition(map);
         Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
         Console.Write(" \b");
         Console.Write("$");
-        Console.SetCursorPosition(20, 9);
+        Console.SetCursorPosition(1, 1);
         int score = 0;
         while (true)
         {
@@ -54,21 +53,58 @@ public class EventLoop
                 Console.Write($"{score}");
                 Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
                 
-                (rowNumberCoin, columnNumberCoin) = GetRandomPosition(walls);
+                (rowNumberCoin, columnNumberCoin) = GetRandomPosition(map);
                 Console.SetCursorPosition(columnNumberCoin, rowNumberCoin);
                 Console.Write(" \b");
                 Console.Write("$");
+                if (score == 10)
+                {
+                    PrintFirework();
+                    Console.SetCursorPosition(0, 22);
+                    if ((Convert.ToInt16(mapPath[mapPath.IndexOf('l') + 1]) - 48) != 3 && !customMap)
+                    {
+                        Console.WriteLine("Для перехода на другой уровень нажмите Enter.");
+                        Console.ReadLine();
+                    }
+                    if (mapPath[mapPath.IndexOf('l') + 1] != '3' && !customMap)
+                    {
+                        Game(mapPath[..(mapPath.IndexOf('l') + 1)] + (Convert.ToInt16(mapPath[mapPath.IndexOf('l') + 1]) - 47).ToString() + ".txt", false);
+                        return;
+                    }
+                }
                 Console.SetCursorPosition(currentColumn, currentRow);
             }
             var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.R)
+            {
+                Game(mapPath, customMap);
+                return;
+            }
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Console.SetCursorPosition(0, 22);
+                return;
+            }
             if (key.Key == ConsoleKey.LeftArrow)
             {
                 (int left, int top) = Console.GetCursorPosition();
                 if (walls[top, left - 1] == 1)
                 {
                     Console.SetCursorPosition(0, 22);
-                    Console.WriteLine("Вы проиграли");
-                    break;
+                    Console.WriteLine("Вы проиграли, нажмите R, чтобы начать уровень заново или Esc, чтобы выйти из игры.");
+                    while (true)
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.R)
+                        {
+                            Game(mapPath, customMap);
+                            return;
+                        }
+                        else if (key.Key == ConsoleKey.Escape)
+                        {
+                            return;
+                        }
+                    }
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left - 1, top);
@@ -81,8 +117,20 @@ public class EventLoop
                 if (walls[top, left + 1] == 1)
                 {
                     Console.SetCursorPosition(0, 22);
-                    Console.WriteLine("Вы проиграли");
-                    break;
+                    Console.WriteLine("Вы проиграли, нажмите R, чтобы начать уровень заново или Esc, чтобы выйти из игры.");
+                    while (true)
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.R)
+                        {
+                            Game(mapPath, customMap);
+                            return;
+                        }
+                        else if (key.Key == ConsoleKey.Escape)
+                        {
+                            return;
+                        }
+                    }
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left + 1, top);
@@ -95,8 +143,20 @@ public class EventLoop
                 if (walls[top - 1, left] == 1)
                 {
                     Console.SetCursorPosition(0, 22);
-                    Console.WriteLine("Вы проиграли");
-                    break;
+                    Console.WriteLine("Вы проиграли, нажмите R, чтобы начать уровень заново или Esc, чтобы выйти из игры.");
+                    while (true)
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.R)
+                        {
+                            Game(mapPath, customMap);
+                            return;
+                        }
+                        else if (key.Key == ConsoleKey.Escape)
+                        {
+                            return;
+                        }
+                    }
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left, top - 1);
@@ -109,8 +169,20 @@ public class EventLoop
                 if (walls[top + 1, left] == 1)
                 {
                     Console.SetCursorPosition(0, 22);
-                    Console.WriteLine("Вы проиграли");
-                    break;
+                    Console.WriteLine("Вы проиграли, нажмите R, чтобы начать уровень заново или Esc, чтобы выйти из игры.");
+                    while (true)
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.R)
+                        {
+                            Game(mapPath, customMap);
+                            return;
+                        }
+                        else if (key.Key == ConsoleKey.Escape)
+                        {
+                            return;
+                        }
+                    }
                 }
                 Console.Write(" \b \b");
                 Console.SetCursorPosition(left, top + 1);
@@ -119,17 +191,27 @@ public class EventLoop
             }
         }
     }
-    private static (int, int) GetRandomPosition(int[,] walls)
+    private static (int, int) GetRandomPosition(string[] map)
     {
         Random randomNumeber = new();
-        var rowNumber = randomNumeber.Next(1, 18);
-        var columnNumber = randomNumeber.Next(1, 40);
-        while (walls[rowNumber, columnNumber] != 0)
+        var rowNumber = randomNumeber.Next(1, map.Length - 2);
+        var columnNumber = randomNumeber.Next(1, map[0].Length - 2);
+        while (map[rowNumber][columnNumber] != ' ')
         {
-            rowNumber = randomNumeber.Next(1, 18);
-            columnNumber = randomNumeber.Next(1, 40);
+            rowNumber = randomNumeber.Next(1, map.Length - 2);
+            columnNumber = randomNumeber.Next(1, map[0].Length - 2);
         }
         return (rowNumber, columnNumber);
 
+    }
+
+    private static void PrintFirework()
+    {
+        var firework = File.ReadAllLines("../../../FireWork.txt");
+        for (int i = 0; i < 7; ++i)
+        {
+            Console.SetCursorPosition(45, 5 + i);
+            Console.Write(firework[i]);
+        }
     }
 }
