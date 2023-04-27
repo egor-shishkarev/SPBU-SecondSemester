85a60406c7a55868f6b86bf490afe7539ed32669
egor-shishkarev <egorsiskarev92@gmail.com>
2023-04-27 23:38:12 +0300

    Add Calculator Form

---
+++ Homework7/Calculator/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Number1 = new Button();
+            Number2 = new Button();
+            Number3 = new Button();
+            Number4 = new Button();
+            Number5 = new Button();
+            Number6 = new Button();
+            Number7 = new Button();
+            Number8 = new Button();
+            Number9 = new Button();
+            Number0 = new Button();
+            PlusButton = new Button();
+            MinusButton = new Button();
+            MultiplyButton = new Button();
+            DivisionButton = new Button();
+            DeleteButton = new Button();
+            EqualButton = new Button();
+            outputWindow = new TextBox();
+            SuspendLayout();
+            // 
+            // Number1
+            // 
+            Number1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number1.Location = new Point(12, 156);
+            Number1.Name = "Number1";
+            Number1.Size = new Size(80, 80);
+            Number1.TabIndex = 0;
+            Number1.Text = "1";
+            Number1.UseVisualStyleBackColor = true;
+            // 
+            // Number2
+            // 
+            Number2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number2.Location = new Point(98, 156);
+            Number2.Name = "Number2";
+            Number2.Size = new Size(80, 80);
+            Number2.TabIndex = 1;
+            Number2.Text = "2";
+            Number2.UseVisualStyleBackColor = true;
+            // 
+            // Number3
+            // 
+            Number3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number3.Location = new Point(184, 156);
+            Number3.Name = "Number3";
+            Number3.Size = new Size(80, 80);
+            Number3.TabIndex = 2;
+            Number3.Text = "3";
+            Number3.UseVisualStyleBackColor = true;
+            // 
+            // Number4
+            // 
+            Number4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number4.Location = new Point(12, 242);
+            Number4.Name = "Number4";
+            Number4.Size = new Size(80, 80);
+            Number4.TabIndex = 3;
+            Number4.Text = "4";
+            Number4.UseVisualStyleBackColor = true;
+            // 
+            // Number5
+            // 
+            Number5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number5.Location = new Point(98, 242);
+            Number5.Name = "Number5";
+            Number5.Size = new Size(80, 80);
+            Number5.TabIndex = 4;
+            Number5.Text = "5";
+            Number5.UseVisualStyleBackColor = true;
+            // 
+            // Number6
+            // 
+            Number6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number6.Location = new Point(184, 242);
+            Number6.Name = "Number6";
+            Number6.Size = new Size(80, 80);
+            Number6.TabIndex = 5;
+            Number6.Text = "6";
+            Number6.UseVisualStyleBackColor = true;
+            // 
+            // Number7
+            // 
+            Number7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number7.Location = new Point(12, 328);
+            Number7.Name = "Number7";
+            Number7.Size = new Size(80, 80);
+            Number7.TabIndex = 6;
+            Number7.Text = "7";
+            Number7.UseVisualStyleBackColor = true;
+            // 
+            // Number8
+            // 
+            Number8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number8.Location = new Point(98, 328);
+            Number8.Name = "Number8";
+            Number8.Size = new Size(80, 80);
+            Number8.TabIndex = 7;
+            Number8.Text = "8";
+            Number8.UseVisualStyleBackColor = true;
+            // 
+            // Number9
+            // 
+            Number9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number9.Location = new Point(184, 328);
+            Number9.Name = "Number9";
+            Number9.Size = new Size(80, 80);
+            Number9.TabIndex = 8;
+            Number9.Text = "9";
+            Number9.UseVisualStyleBackColor = true;
+            // 
+            // Number0
+            // 
+            Number0.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Number0.Location = new Point(98, 414);
+            Number0.Name = "Number0";
+            Number0.Size = new Size(80, 80);
+            Number0.TabIndex = 9;
+            Number0.Text = "0";
+            Number0.UseVisualStyleBackColor = true;
+            // 
+            // PlusButton
+            // 
+            PlusButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            PlusButton.Location = new Point(270, 242);
+            PlusButton.Name = "PlusButton";
+            PlusButton.Size = new Size(80, 80);
+            PlusButton.TabIndex = 10;
+            PlusButton.Text = "+";
+            PlusButton.UseVisualStyleBackColor = true;
+            // 
+            // MinusButton
+            // 
+            MinusButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            MinusButton.Location = new Point(270, 328);
+            MinusButton.Name = "MinusButton";
+            MinusButton.Size = new Size(80, 80);
+            MinusButton.TabIndex = 11;
+            MinusButton.Text = "-";
+            MinusButton.UseVisualStyleBackColor = true;
+            // 
+            // MultiplyButton
+            // 
+            MultiplyButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            MultiplyButton.Location = new Point(12, 414);
+            MultiplyButton.Name = "MultiplyButton";
+            MultiplyButton.Size = new Size(80, 80);
+            MultiplyButton.TabIndex = 12;
+            MultiplyButton.Text = "*";
+            MultiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // DivisionButton
+            // 
+            DivisionButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DivisionButton.Location = new Point(184, 414);
+            DivisionButton.Name = "DivisionButton";
+            DivisionButton.Size = new Size(80, 80);
+            DivisionButton.TabIndex = 13;
+            DivisionButton.Text = "/";
+            DivisionButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            DeleteButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            DeleteButton.Location = new Point(270, 156);
+            DeleteButton.Name = "DeleteButton";
+            DeleteButton.Size = new Size(80, 80);
+            DeleteButton.TabIndex = 14;
+            DeleteButton.Text = "C";
+            DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // EqualButton
+            // 
+            EqualButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            EqualButton.Location = new Point(270, 414);
+            EqualButton.Name = "EqualButton";
+            EqualButton.Size = new Size(80, 80);
+            EqualButton.TabIndex = 15;
+            EqualButton.Text = "=";
+            EqualButton.UseVisualStyleBackColor = true;
+            // 
+            // outputWindow
+            // 
+            outputWindow.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            outputWindow.Location = new Point(12, 86);
+            outputWindow.Multiline = true;
+            outputWindow.Name = "outputWindow";
+            outputWindow.ReadOnly = true;
+            outputWindow.Size = new Size(338, 52);
+            outputWindow.TabIndex = 16;
+            outputWindow.Text = "0";
+            outputWindow.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(362, 503);
+            Controls.Add(outputWindow);
+            Controls.Add(EqualButton);
+            Controls.Add(DeleteButton);
+            Controls.Add(DivisionButton);
+            Controls.Add(MultiplyButton);
+            Controls.Add(MinusButton);
+            Controls.Add(PlusButton);
+            Controls.Add(Number0);
+            Controls.Add(Number9);
+            Controls.Add(Number8);
+            Controls.Add(Number7);
+            Controls.Add(Number6);
+            Controls.Add(Number5);
+            Controls.Add(Number4);
+            Controls.Add(Number3);
+            Controls.Add(Number2);
+            Controls.Add(Number1);
+            Name = "Form1";
+            Text = "Калькулятор";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Number1;
+        private Button Number2;
+        private Button Number3;
+        private Button Number4;
+        private Button Number5;
+        private Button Number6;
+        private Button Number7;
+        private Button Number8;
+        private Button Number9;
+        private Button Number0;
+        private Button PlusButton;
+        private Button MinusButton;
+        private Button MultiplyButton;
+        private Button DivisionButton;
+        private Button DeleteButton;
+        private Button EqualButton;
+        private TextBox outputWindow;
+    }
+}
