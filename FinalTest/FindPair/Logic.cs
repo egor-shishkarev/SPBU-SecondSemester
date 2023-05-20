24493fd495f68c6ce1834fae738740bd200eb2ce
egor-shishkarev <egorsiskarev92@gmail.com>
2023-05-20 12:33:50 +0300

    Add the appearance and deletion of text on the buttons, saving the text with a right choice.

--- FinalTest/FindPair/Logic.cs
+++ FinalTest/FindPair/Logic.cs
@@ -4,12 +4,18 @@ public class Logic
 {
     int[] tableOfNumbers;
 
+    private List<Button> listOfCards = new List<Button>();
+
+    public int GetCount => listOfCards.Count;
+
+    private int countOfOpenCards = 0;
+
     public Logic(int number)
     {
         int numberOfPairs = (int)Math.Pow(number, 2) / 2 - 1;
         var tableOfNumbers = new int[number * number];
         var listOfNumbers = new List<int>();
-        for (int i = 0; i < numberOfPairs; ++i)
+        for (int i = 0; i <= numberOfPairs; ++i)
         {
             listOfNumbers.Add(i);
             listOfNumbers.Add(i);
@@ -18,7 +24,7 @@ public class Logic
         {
             for (int j = 0; j < number; ++j)
             {
-                var currentElement = listOfNumbers[new Random().Next(0, listOfNumbers.Count - 1)];
+                var currentElement = listOfNumbers[new Random().Next(0, listOfNumbers.Count)];
                 listOfNumbers.Remove(currentElement);
                 tableOfNumbers[j * number + i] = currentElement;
             }
@@ -28,12 +34,30 @@ public class Logic
 
     public void OpenCard(object sender)
     {
+        if (listOfCards.Count == 2)
+        {
+            if (listOfCards[0].Text == listOfCards[1].Text)
+            {
+                listOfCards.Clear();
+                ++countOfOpenCards;
+                if (countOfOpenCards == tableOfNumbers.Count())
+                {
+                    Application.Exit();
+                }
+            }
+            CloseCards();
+        }
         var _sender = sender as Button;
         _sender.Text = tableOfNumbers[_sender.TabIndex].ToString();
+        listOfCards.Add(_sender);
     }
 
-    public void Close()
+    public void CloseCards()
     {
-
+        foreach (var button in listOfCards)
+        {
+            button.Text = "";
+        }
+        listOfCards.Clear();
     }
 }
