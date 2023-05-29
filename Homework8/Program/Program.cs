51d57ef6835970db9f985d252780c9f5d988ae1b
egor-shishkarev <egorsiskarev92@gmail.com>
2023-05-29 16:21:54 +0300

    Add program for using SkipList

--- Homework8/Program/Program.cs
+++ Homework8/Program/Program.cs
@@ -1,6 +1,13 @@
 ﻿using SkipList;
 
 var list = new SkipList<int>();
-
-list.Add(1);
-Console.WriteLine(list.Contains(1));
+list.Add(30);
+list.Add(40);
+list.Add(50);
+list.Add(60);
+list.Add(70);
+list.Add(90);
+foreach (var item in list)
+{
+    Console.Write($"{item} ");
+}
